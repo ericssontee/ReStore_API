fe8f2bb17c1ee69c358bbecf985ea1f101e1b5ee
ericssontee <ericssontee.eb@gmail.com>
2023-08-04 09:55:49 +0800

    Add api products Search function

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -16,11 +16,12 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts(string orderBy)
+        public async Task<ActionResult<List<Product>>> GetProducts(string orderBy, string searchTerm)
         {
             // NOTE AFTER THE COURSE: CHECK REPOSITORY PATTERN
             var query = _context.Products
                 .Sort(orderBy)
+                .Search(searchTerm)
                 .AsQueryable();
 
             return await query.ToListAsync();
